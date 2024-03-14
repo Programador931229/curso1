bbc2657c2b2b0b65db4835fd92d15574a719a638
Ricardo Diaz <rd.diazvega@ugto.mx>
2024-03-14 09:17:01 -0600

    creacion de formulario en vista create

--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -4,4 +4,26 @@
 
 @section('content')
     <h1>Aqui podras crear un nuevo curso</h1>
+    <form action="" method="post">
+        <label>
+            Nombre:
+            <br>
+            <input type="text" name="nombre">
+        </label>
+        <br>
+        <label>
+            Descripcion
+            <br>
+            <textarea name="descripcion" rows="5"></textarea>
+        </label>
+        <br>
+        <label>
+            Clasificacion
+            <input type="text" name="clasificacion">
+        </label>
+        <br>
+        <button>
+            Guardar
+        </button>
+    </form>
 @endsection
